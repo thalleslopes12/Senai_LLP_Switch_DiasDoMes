ee101ea5ff366bc24492f45a36d323c71a499102
thalleslopes12 <thallesmerencio12@gmail.com>
2025-06-14 12:27:40 -0300

    Update teste.java
    Esqueci de apagar esse arquivo

--- teste.java
+++ teste.java
@@ -1,14 +1,1 @@
-public class teste {
-    public static void main(String[] args) {
-        String numeroString = "pao"; // String que queremos converter
-        int numeroInt;
 
-        // Conversão de String para int
-        try {
-            numeroInt = Integer.parseInt(numeroString);
-            System.out.println("A string '" + numeroString + "' foi convertida para o inteiro: " + numeroInt);
-        } catch (NumberFormatException e) {
-            System.out.println("Erro: A string não pode ser convertida para um inteiro.");
-        }
-    }
-}
